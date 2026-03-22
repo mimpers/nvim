6b38585fe04dff80e911d816078e4c7e6faf1f20
mimpers <pwc.crawford@gmail.com>
2026-03-21 22:14:17 -0400

    add indent-blankline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,19 @@
 -- Import all plugin specs
 return {
-    require("plugins.colorscheme"),   -- load colors first
-    require("plugins.treesitter"),    -- syntax highlighting ready
-    require("plugins.todo-comments"), -- highlights can use treesitter
-    require("plugins.neo-tree"),      -- buffer highlights and icons are ready
-    require("plugins.lsp"),           -- LSP attaches after treesitter
-    require("plugins.telescope"),     -- pickers using LSP will work
-    require("plugins.harpoon"),
-    require("plugins.gitsigns"),      -- git features
-    require("plugins.guess-indent"),  -- indentation detection
-    require("plugins.which-key"),     -- keybinding hints
-    require("plugins.conform"),       -- autoformatting (LSP already loaded)
-    require("plugins.blink"),         -- completion/snippets
-    require("plugins.misc"),          -- other utilities
-    require("plugins.toggleterm"),    -- terminal
-    require("plugins.autopairs"),
+  require 'plugins.colorscheme', -- load colors first
+  require 'plugins.treesitter', -- syntax highlighting ready
+  require 'plugins.todo-comments', -- highlights can use treesitter
+  require 'plugins.neo-tree', -- buffer highlights and icons are ready
+  require 'plugins.lsp', -- LSP attaches after treesitter
+  require 'plugins.telescope', -- pickers using LSP will work
+  require 'plugins.harpoon',
+  require 'plugins.gitsigns', -- git features
+  require 'plugins.guess-indent', -- indentation detection
+  require 'plugins.which-key', -- keybinding hints
+  require 'plugins.conform', -- autoformatting (LSP already loaded)
+  require 'plugins.blink', -- completion/snippets
+  require 'plugins.misc', -- other utilities
+  require 'plugins.toggleterm', -- terminal
+  require 'plugins.autopairs',
+  require 'plugins.indent-blankline',
 }
